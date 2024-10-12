ed1bb5c4aeb7e0b78990aa7c2999f70a7a2c87dd
pvp166 <phuchp1997@gmail.com>
2024-10-12 22:23:24 +0700

    fix bug

--- middleware/auth.global.ts
+++ middleware/auth.global.ts
@@ -7,11 +7,18 @@ export default defineNuxtRouteMiddleware(async (to, from) => {
   ) {
     return;
   }
-  const { data: authStatus, error } = await useAPI("custom/v1/permission", {
-    credentials: "include",
-  });
-  console.log(error, !authStatus, authStatus.value.loggedIn);
-  if (error.value || !authStatus || authStatus.value.loggedIn === false) {
-    return navigateTo("/login");
+  if (import.meta.server) {
+    return
   }
+  // skip middleware on client side entirely
+  if (import.meta.client) {
+    const { data: authStatus, error } = await useAPI("custom/v1/permission", {
+      credentials: "include",
+    });
+    console.log(error, !authStatus, authStatus.value);
+    if (error.value || !authStatus || authStatus.value.loggedIn === false) {
+      return navigateTo("/login");
+    }
+  }
+ 
 });
