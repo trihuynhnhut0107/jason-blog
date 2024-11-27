a07b3ca2efad20b557fa20a20320f3b14f1e947b
pvp166 <vandev1668@gmail.com>
2024-11-27 21:13:38 +0700

    update data

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,85 @@
 <template>
   <div class="space-y-6">
-   
+    <PostUnlockConfirmation
+      v-model:popupActive="purchasingPopupActive"
+      v-model:purchasingPost="confirmPurchasingPost"
+      v-model:currentPost="currentPost" />
+
+    <div class="flex flex-row space-x-2 justify-center items-center w-full">
+      <div v-for="(category, index) in categoryList">
+        <div
+          v-if="category.children.length !== 0"
+          class="relative inline-block"
+          @click="changeDropdownVisibility(category, !category.isOpen)"
+          @mouseenter="changeDropdownVisibility(category, true)"
+          @mouseleave="changeDropdownVisibility(category, false)">
+          <!-- Dropdown Button -->
+          <button
+            class="inline-flex justify-center w-full shadow-sm px-4 py-2 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none"
+            :class="`${category.isOpen ? 'border-b-2 border-black' : ''}`">
+            {{ category.parent.name }}
+          </button>
+          <!-- Dropdown Menu -->
+          <div
+            v-if="category.isOpen"
+            class="absolute w-24 text-sm md:w-24 md:text-base text-start rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 focus:outline-none">
+            <div class="py-1 flex flex-col" role="menu">
+              <div v-for="subCategory in category.children" class="px-4 py-2">
+                <button
+                  class="text-start w-full"
+                  :class="`${subCategory.isSelected ? 'text-red-text' : ''}`"
+                  @click="changeCategory(subCategory)">
+                  {{ subCategory.name }}
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div v-else>
+          <button
+            class="inline-flex justify-center w-full shadow-sm px-4 py-2 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none"
+            :class="`${
+              category.isSelected
+                ? 'text-red-text border-b-2 border-red-text'
+                : ''
+            }`"
+            @click="changeCategory(category.parent)">
+            {{ category.parent.name }}
+          </button>
+        </div>
+      </div>
+    </div>
+    <div
+      v-if="
+        (data?.data?.posts && data?.data?.total_pages > 0) || status === 'pending'
+      "
+      class="flex flex-col">
+      <div
+        v-for="item in data.data.posts"
+        :key="item.id"
+        class="flex flex-col py-4">
+        
+        <button
+          class="space-y-3 hover:text-red-text"
+          @click="navigateToPost(item)">
+          <PostCard :post="item"/>
+        </button>
+      </div>
+
+      <div
+        class="flex flex-row items-center justify-center space-x-1 text-center">
+        Page:
+        <input
+          type="text"
+          v-model="currentPageNumber"
+          class="text-center w-12 border-2 p-1 m-1 rounded-md" />
+        /
+        {{ data.data.total_pages }}
+      </div>
+    </div>
+    <div v-else>
+      <h1>No available post</h1>
+    </div>
   </div>
 </template>
 
